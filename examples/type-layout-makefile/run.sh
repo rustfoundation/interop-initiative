e88839b55333998527802b1bce12cd25f4965449
Brace1000 <oikobrian99@gmail.com>
2026-03-25 13:50:12 +0300

    Add run.sh to type-layout-makefile for CI

--- examples/type-layout-makefile/run.sh
+++ examples/type-layout-makefile/run.sh
@@ -1,3 +1,16 @@
-#!/bin/sh
-set -e
+#!/usr/bin/env bash
+# ^ support bash that's not in the standard `/bin` path
+
+# A script that builds and runs the type-layout-makefile example
+
+# This file needs to be executable:
+# - on Linux or macOS, use `chmod +x run.sh` then `git add run.sh`
+# - on Windows, use `git add --chmod=+x run.sh` or `git update-index --chmod=+x run.sh`
+
+# bash "strict mode": fail the script if any command fails, or any variable is unset when used
+set -euo pipefail
+
+# If the script is run outside the example directory, change to the expected location
+cd "$(dirname "$0")"
+
 make all
