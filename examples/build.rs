55f95befd97ca90a53dc6dc4aa21e2c600da67e3
yrevash9182 <yrevash@gmail.com>
2026-03-25 12:29:52 +0530

    fmt

--- examples/build.rs
+++ examples/build.rs
@@ -2,7 +2,7 @@
 // so that rust can link against it later
 fn main() {
     cc::Build::new()
-        .cpp(true)           // we're compiling c++ not plain c
-        .file("main.cpp")   // our c++ file with the wrapper functions
+        .cpp(true) // we're compiling c++ not plain c
+        .file("main.cpp") // our c++ file with the wrapper functions
         .compile("greetings"); // output lib name, rust will find it automatically
 }
