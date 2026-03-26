eafdd14b28e8960ab335085405d2f0bc29217357
JojoFlex1 <whateveriffeel@gmail.com>
2026-03-26 12:17:44 +0300

    Restructure to use cargo instead of rustc directly

--- examples/fibonacci-example/run.sh
+++ examples/fibonacci-example/run.sh
@@ -1,2 +1,11 @@
-#!/bin/bash
-make run
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+cd rust-library && cargo build
+cd ..
+
+g++ -o fibonacci_demo main.cpp rust-library/target/debug/libfibonacci.a -lpthread -ldl
+
+./fibonacci_demo
