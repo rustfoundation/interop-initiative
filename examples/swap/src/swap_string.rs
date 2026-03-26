1e4962acec84554f2e9736bfb93fd838d5cce4fd
Pavan <pavankumaryalagada@gmail.com>
2026-03-26 11:05:16 +0530

    fix: mark function unsafe, use ptr::swap, and add safety docs

--- examples/swap/src/swap_string.rs
+++ examples/swap/src/swap_string.rs
@@ -1,10 +1,11 @@
 use std::os::raw::c_char;
 
 #[no_mangle]
-pub extern "C" fn swap_string(a: *mut *mut c_char, b: *mut *mut c_char) {
-    unsafe {
-	let temp = *a;
-	*a = *b;
-	*b = temp;
-	}
+/// # Safety
+///
+/// a and b must be valid, non null pointers.
+/// They must point to valid memory containing *must_char.
+/// The memory must be properly aligned and writable.
+pub unsafe extern "C" fn swap_string(a: *mut *mut c_char, b: *mut *mut c_char) {
+    core::ptr::swap(a, b);
 }
