04a0d6a5495c87e77d639597e16d438b2b3d1cd6
Kelechi Ebiri <ebiritg@gmail.com>
2026-03-28 12:09:04 +0100

    Extend example to support Rust -> C++ calls
    Signed-off-by: Kelechi Ebiri <ebiritg@gmail.com>

--- examples/cpp-calls-rust/cpp/main.cpp
+++ examples/cpp-calls-rust/cpp/main.cpp
@@ -3,6 +3,12 @@
 // Declare the Rust function using C ABI
 extern "C" int add(int left, int right);
 
+
+// Expose C++ function to Rust
+extern "C" int multiply(int a, int b) {
+	return a * b;
+}
+
 int main() {
 	std::cout << "2 + 3 = " << add(2, 3) << std::endl;
 	return 0;
--- examples/cpp-calls-rust/rust/src/lib.rs
+++ examples/cpp-calls-rust/rust/src/lib.rs
@@ -1,8 +1,16 @@
+// Tell Rust that this function exists in C++
+unsafe extern "C" {
+    fn multiply(a: i32, b: i32) -> i32;
+}
+
+
 // Expose this function to C++ using the C ABI.
 // `no_mangle` is marked unsafe because it controls the symbol name at link time.
 #[unsafe(no_mangle)]
 pub extern "C" fn add(left: i32, right: i32) -> i32 {
-    left + right
+    let result = unsafe { multiply(left, right) };
+
+    left + right + result
 }
 
 #[cfg(test)]
@@ -12,6 +20,6 @@ mod tests {
     #[test]
     fn it_works() {
         let result = add(2, 3);
-        assert_eq!(result, 5);
+        assert_eq!(result, 11);
     }
 }
